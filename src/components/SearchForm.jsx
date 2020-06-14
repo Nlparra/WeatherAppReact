778f71dca09bf8f3f89e79bae835c49c57b3652b
Nelson Parra <nlparra.dev@gmail.com>
2020-06-14 16:58:18 -0400

    work on wave pattern , logo and search styling

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,23 +4,24 @@ import { Row,Col,Form,Button} from 'react-bootstrap';
 
 const SearchForm = props => {
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    props.setUserInput(event.target.query.value)
-    event.target.reset()
-  }
+  // const handleSubmit = event => {
+  //   event.preventDefault()
+  //   props.setUserInput(event.target.query.value)
+  //   event.target.reset()
+  // }
 
   return(
 
     <Form className="search" >
+      
   <Row>
     <Col>
       <Form.Control  name="query" placeholder="Search for city" />
     </Col>
     <Col>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
+      {/* <Button variant="primary" type="submit" onClick={handleSubmit}>
     Search
-  </Button>
+  </Button> */}
     </Col>
   </Row>
 </Form>
