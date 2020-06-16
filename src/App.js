20faff3da76a30511122cafee7ca3f88ba33bda2
Nelson Parra <nlparra.dev@gmail.com>
2020-06-16 18:43:06 -0400

    set route to link to correct city , create fetch to obtain forecast - need to display the information

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Main from "./components/Main"
 import City from "./components/City"
 
 const App = () => {
   return (
     <Router>
-      <Route exact path='/'>
-        <Main />
-      </Route>
-      <Route path="/city/:cityId">
-          <City />
-      </Route>
+      <Route exact path='/' component={Main}/>
+      <Route path="/city/:cityId" component={City}/>
+      <Route render={() => <h1>Not found!</h1>} />
     </Router>
   );
 }
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,23 +1,41 @@
-import React, { useEffect, useState ,Fragment} from 'react'
+import React, {Fragment,useEffect,useState} from 'react'
+
 
 
 const City = props => {
 
+  const [forecast, setForecast] = useState({});
+
   
 
+  useEffect(() => {
+    fetch(`https://wyn-weather-api.herokuapp.com/cities/${props.match.params.cityId}`)
+      .then(res => res.json())
+      .then(results => setForecast(results))
+      .catch(err => console.log(err))
+  })
+
+
+
+console.log(forecast)
   
   return(
     <Fragment>
     <div className="container"> 
       <div className="inner-container">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#eeaeca" fillOpacity="1" d="M0,192L80,165.3C160,139,320,85,480,80C640,75,800,117,960,149.3C1120,181,1280,203,1360,213.3L1440,224L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-  
-          
-        </div>
-    </div>    
-  </Fragment>
- 
-  )
-}
+      </div>
+    
+           {
+             forecast.map(day => {
+                return (console.log(day))
+
+             })}
+
+
+
+         </div>
+    </Fragment>
+  )}
 
 export default City
